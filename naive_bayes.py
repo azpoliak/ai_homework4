e994df36f9b9d92d6cea5241e58518163ede0667
azpoliak <azpoliak@gmail.com>
2015-11-27 11:19:59 -0500

    naive bayes: Calculate std and mean for training

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,11 +1,13 @@
 """
 Class for a classification algorithm.
 """
-
+import pdb
 import numpy as np
+import math
 
 class Naive_Bayes:
-
+ 
+        
 	def __init__(self, classifier_type, **kwargs):
 		"""
 		Initializer. Classifier_type should be a string which refers
@@ -22,11 +24,12 @@ class Naive_Bayes:
 		"""
 		self.classifier_type = classifier_type
 		self.params = kwargs
+		self.mean = {}
+		self.stdv = {}
 		"""
 		The kwargs you inputted just becomes a dictionary, so we can save
 		that dictionary to be used in other methods.
 		"""
-
 
 	def train(self, training_data):
 		"""
@@ -48,19 +51,42 @@ class Naive_Bayes:
 
 		You should print the accuracy, precision, and recall on the training data.
 		"""
-
+                #seperate by class
+                classify = {}
+                for row in training_data:
+                    #print row
+                    if row[0] not in classify:
+                      classify[row[0]] = [row[1:]]
+                    else:
+                      #pdb.set_trace()
+                      classify[row[0]].append(row[1:])
+                #return classify
+                # get mean for each attribute per class
+                
+                #pdb.set_trace()
+                for clas in classify.keys():
+                	for i in range(len(classify[clas][0])):
+                		self.mean[(clas, i)] = np.mean(zip(*classify[clas])[i])
+                		self.stdv[(clas, i)] = np.std(zip(*classify[clas])[i])
+                #pdb.set_trace()
 	def predict(self, data):
 		"""
 		Predict class of a single data vector
 		Data should be 1x(m+1) numpy matrix where m is the number of features
 		(recall that the first element of the vector is the label).
-
+        
 		I recommend implementing the specific algorithms in a
 		seperate module and then determining which method to call
 		based on classifier_type.
 
 		This method should return the predicted label.
 		"""
+		realClass = data[0]
+		attributes = data[1:]
+
+		#get probablity for each attribute
+		for i in range(len(attributes)):
+			print i 
 
 	def test(self, test_data):
 		"""
