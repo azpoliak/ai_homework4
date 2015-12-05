d8055d092e19a81e3111475066c92b8b1cbdd31b
Sayge Schell <schell@jhu.edu>
2015-12-05 17:16:09 -0500

    fixed recall + precision, updated data

--- decision_tree_driver.py
+++ decision_tree_driver.py
@@ -3,67 +3,54 @@ from naive_bayes import Naive_Bayes
 import load_data as ld
 import pdb
 
-nb = Decision_Tree("decision_tree", pruning=False, info_gain_ratio=True)
+#nb = Decision_Tree("decision_tree", pruning=False, info_gain_ratio=True)
+nb = Naive_Bayes("naive_bayes")
 
-#congress = ld.load_monks(3)
-#congress = ld.load_monks(2)
-#congress = ld.load_monks(3)
-#congress = ld.load_congress(.75)
-congress = ld.load_congress_data(.75)
+#monks3 = ld.load_monks(1)
+#monks3 = ld.load_monks(2)
+#monks3 = ld.load_monks(3)
+#monks3 = ld.load_iris(.75)
+monks3 = ld.load_congress_data(.75)
 
-#nb.train(congress[0])
+#nb.train(monks3[0])
 """tot, hit = 0, 0
-for person in congress[1]:
+for person in monks3[1]:
   predict = nb.predict(person)
   if predict == person[0]:
   	hit += 1
   tot += 1"""
 
-#classify =  nb.train(congress[0])
+#classify =  nb.train(monks3[0])
 
-nb.train(congress[0])
-#nb.train(congress[0])
-#nb.train(congress[0])
-#nb.train(congress[0])
+nb.train(monks3[0])
+#nb.train(monks3[0])
+#nb.train(monks3[0])
+#nb.train(monks3[0])
 #pdb.set_trace()
-#nb.test(congress[1])
+#nb.test(monks3[1])
 
-
-#Accuracy
-tot, hit = 0, 0
-for person in congress[1]:
+#Accuracy, Recall, and Precision
+relevant_and_retrieved, relevant, retrieved, total, hit = 0, 0, 0, 0, 0
+for person in monks3[1]:
   predict = nb.predict(person)
+  if predict == monks3[1][0][0] and person[0] == monks3[1][0][0]:
+  	relevant_and_retrieved += 1
+  if person[0] == monks3[1][0][0]:
+        relevant += 1
+  if predict == monks3[1][0][0]:
+        retrieved += 1
   if predict == person[0]:
-  	hit += 1
-  tot += 1
-accuracy = hit/float(tot)
+        hit += 1
+  total += 1
+accuracy = hit/float(total)
+recall = relevant_and_retrieved/float(relevant)
+precision = relevant_and_retrieved/float(retrieved)
 print "Accuracy: ", accuracy
-
-#Recall
-id_pos, actual_pos = 0, 0
-for person in congress[1]:
-  predict = nb.predict(person)
-  if predict == congress[1][0][0]:
-  	id_pos += 1
-  if person[0] == congress[1][0][0]:
-        actual_pos += 1
-  tot += 1
-recall = id_pos/float(actual_pos)
+print "Precision: ", precision
 print "Recall: ", recall
 
-
-#Precision
-total, p = 0, 0
-for person in congress[1]:
-  predict = nb.predict(person)
-  if predict == congress[1][0][0]:
-    p += 1
-  total += 1
-precision = id_pos/float(total)
-print "Precision: ", precision
-
 """tot, hit = 0, 0
-for person in congress[1]:
+for person in monks3[1]:
   predict = nb.predict(person)
   if predict == person[0]:
   	hit += 1
